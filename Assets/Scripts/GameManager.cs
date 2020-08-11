40091972a9a72569602bbdd62fb9eeee038a5dd0
Andrew L. Martinez <44044987+almartinez1@users.noreply.github.com>
2020-08-11 15:05:03 -0700

    implimented if and while loops
    used if and while loops to say amount of coins generated.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int coins = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int coinGenerated = 1;
+
+        /*
+        while (coinGenerated <= coins)
+        {
+            Debug.Log(coinGenerated + " coins generated");
+            coinGenerated++; // coinGenerated = coinGenerated + 1;
+        }
+        */
+
+        for (int i = 1; i <= coins; i++)
+        {
+            Debug.Log(i + " coins generated");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Datatype VariableName = Value;
-    int wheels = 4;
+    public int wheels = 4;
     string playerName = "Carbot";
     int speed = 70;
     bool powerUp = false;
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         Debug.Log("I am Carbot - bringer of justice");
         Debug.Log(playerName + " has " + wheels + " wheels.");
 
@@ -21,6 +22,34 @@ public class PlayerController : MonoBehaviour
 
         wheels = 10;
         Debug.Log(playerName + " got a SUPER upgrade, they now have " + wheels + "wheels.");
+        */
+
+        if (wheels < 4)
+        {
+            Debug.Log("Not enough wheels, the burglar gets away.");
+            speed = 0;
+        }
+        else if (wheels>=4 && wheels <= 6)
+        {
+            Debug.Log("You're in pursuit.");
+            speed = speed + 30;
+        }
+        else if (wheels > 6 && wheels < 10)
+        {
+            Debug.Log("Power Up.");
+            powerUp = true;
+        }
+        else
+        {
+            Debug.Log("SUPER POWER UP.");
+            speed = speed + 100;
+            powerUp = true;
+        }
+
+        if (speed > 100 || powerUp == true)
+        {
+            Debug.Log("You Caught the burglar.");
+        }
     }
 
     // Update is called once per frame
