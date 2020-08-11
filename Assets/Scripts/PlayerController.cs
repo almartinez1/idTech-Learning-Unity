5448d9667c4a62c3e220f5de600fca0342b91036
Andrew L. Martinez <44044987+almartinez1@users.noreply.github.com>
2020-08-11 13:59:20 -0700

    used strings and variables to say things.
    Made it say a series of things.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Datatype VariableName = Value;
+    int wheels = 4;
+    string playerName = "Carbot";
+    int speed = 70;
+    bool powerUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am Carbot - bringer of justice");
+        Debug.Log(playerName + " has " + wheels + " wheels.");
+
+        wheels = wheels + 2;
+        Debug.Log(playerName + " got an upgrade, they now have " + wheels + " wheels.");
+
+        wheels = 10;
+        Debug.Log(playerName + " got a SUPER upgrade, they now have " + wheels + "wheels.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
