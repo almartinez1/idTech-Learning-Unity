fe114a1a05e15e6b5a32ed47397a6c3c28a2cd2d
Andrew L. Martinez <44044987+almartinez1@users.noreply.github.com>
2020-08-12 15:27:50 -0700

    Spaceship Sprite Creation
    Included MagicaVoxel files and Spaceship Sprite Creation to local repository folder for Git sync.

---
+++ Assets/Scripts/CodingChallenges.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingChallenges : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //declaration, condition, add or subtract
+        for (int i = 10; i > 0; i--)
+        {
+            Debug.Log(i + "...");
+
+        }
+        Debug.Log("Blastoff.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
